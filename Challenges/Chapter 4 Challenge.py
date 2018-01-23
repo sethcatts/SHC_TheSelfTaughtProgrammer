f194c344fdedb863bf98539140c8fbec0564a4d4
TheFarwolf <seth.h.catts@gmail.com>
2018-01-22 23:51:28 -0500

    Partially finishing chapter 4 notes/challenges

---
+++ Challenges/Chapter 4 Challenge.py
@@ -0,0 +1,30 @@
+#-1. Write a function that takes a number as an input and returns that number squared
+def returnInputSquared(x):
+    return x**2
+
+#-2. Create a function that accepts a string as a parameter and prints it.
+def printString(s):
+    print(s)
+    
+#-3. Write a function that takes three required parameters and two optional parameters
+def manyParaFunction(x,y,z, a = 1, b = 1):
+    return x+y+z**(a+b)
+
+#-4. Write a program with two functions. The first should take an integer as a parameter and return it divided by two. The second function should take a parameter and return it multiplied by four. Save the result of the first function to a variable. Call the second function using the variable assigned by the first
+def fOne(x):
+    return x/2
+def fTwo(x):
+    return x*4
+
+pox = fOne(6)
+fTwo(pox)
+
+#-5. Write a function that converts a string to a float and returns it.
+
+#-6. Add a docstring to all challenges.
+
+
+
+
+
+#<-FUNCTION CALLS->
---
+++ Chapter Notes and Code/Chapter 4 - Functions.py
@@ -0,0 +1,55 @@
+#Python Functions
+
+#Syntax
+
+#def [function name]([parameters]):
+#    [function definition]
+
+#Example
+def funct(x,y,z):
+    return x+y+z
+
+#Assign a variable to a function return
+result = funct(1,2,3)
+print(result)
+
+
+#Built in functions
+len("stringlength")
+str(100) #-> Integer to string example
+int("10000")
+float(100) #-> 100.00
+
+age = input("Input your age:")
+
+#Required and optional parameters
+def retX(x=2):
+    return x
+
+#Scope
+
+#try-catch
+a = input("A:")
+b = input("B:")
+a = int(a)
+b = int(b)
+try:
+    print(a / b)
+except ZeroDivisionError:
+    print("b cannot be zero")
+    
+#Docstrings for functions
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
