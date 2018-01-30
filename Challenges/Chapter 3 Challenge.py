58051654305bd2fe68eab3c0e6eb62f4ed8fb117
TheFarwolf <seth.h.catts@gmail.com>
2018-01-30 09:05:24 -0500

    Fixing max line length of comments. Setting max line length to 80charspaces

--- Challenges/Chapter 3 Challenge.py
+++ Challenges/Chapter 3 Challenge.py
@@ -6,15 +6,18 @@ def print3Strings(x):
     print("String1")
     print("String2")
     print("String3")
-    
-#-2. Write a program that prints a message if a variable is less than 10, and a different message if the variable is greater than or equal to 10.
+
+#-2. Write a program that prints a message if a variable is less than 10, and
+#    a different message if the variable is greater than or equal to 10.
 def tenOrGreater(num):
     if num >= 10:
         print("The number is equal to or greater than 10")
     else:
         print("The input number is less than 10")
-        
-#-3. Write a program that prints a message if a variable is less than or equal to 10, and another if the variable is greater than 10 but less than or equal to 25, and another if the variable is greater than 25
+
+#-3. Write a program that prints a message if a variable is less than or equal
+#    to 10, and another if the variable is greater than 10 but less than or
+#    equal to 25, and another if the variable is greater than 25
 def rangeOfInput(num):
     if num <= 10:
         print("Less than or equal to 10")
@@ -22,13 +25,15 @@ def rangeOfInput(num):
         print("Between 11 and 26")
     else:
         print("Greater than 25")
+
 #-4. Create a program that divides 2 variables and returns the remainder
 def retRemainder(x, y):
     return x % y
-#-5. Create a program that takes two variables divides them and returns the quotient
+#-5. Create a program that takes two variables divides them and returns quotient
 def retQuotient(x, y):
     return x/y
-#-6. Write a program with a variable "age" assigned to an integer that prints different strings depending on the value of the integer
+#-6. Write a program with a variable "age" assigned to an integer that prints
+#    different strings depending on the value of the integer
 def ageProps(age):
     if age < 2:
         print("Baby")
