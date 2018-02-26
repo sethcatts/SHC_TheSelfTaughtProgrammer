5e45eaf1dc19525efaee706fdf304b86bc386481
TheFarwolf <seth.h.catts@gmail.com>
2018-02-26 18:30:08 -0500

    Starting work on chapter 6 reading and notes

---
+++ Chapter Notes and Code/Chapter 6 - String Manipulations.py
@@ -0,0 +1,29 @@
+#Triple Strings
+#if a string spans more than one line you have to put it in triple quotes
+""" Line one
+    Line Two
+    Line Three
+"""
+
+#Indexes
+author = "Young"
+print("Index 0 of author: " + author[0])
+#Negative Indexes
+print("Index -1 of author: " + author[-1])
+
+#Strings are Immutable, create new string with mods
+
+#Concatenation: Add strings together
+
+#String multiplication
+print("mult str" * 3) #-> "mult strmult strmult str"
+
+#Upper and Lower build in METHODS
+
+#Formate Method
+"C.J {}".formate("Young") #-> "C.J Young"
+
+"C.J {}, {} {} {}".formate("Young", "was", "pretty", "smart.")
+#-> "C.J Young was pretty smart"
+
+#Split
