d1784ee7e13c1977ae05a8fd8dfb3515ac3477a4
TheFarwolf <seth.h.catts@gmail.com>
2018-03-01 00:03:29 -0500

    Typing out chapter 6 problem set

---
+++ Challenges/Chapter 6 Challenge.py
@@ -0,0 +1,14 @@
+#-1 Print every character in the string "Camus".
+
+#-2 Write a program that collects two strings from a user, inserts them into
+#   the string "Yesterdy I wrote a [user_input_1]. I sent it to [user_input_2]!"
+#   and prints the new string
+
+#-3
+#-4
+#-5
+#-6
+#-7
+#-8
+#-9
+#-10
