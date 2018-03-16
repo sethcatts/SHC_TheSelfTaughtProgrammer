b488bc2d524ad905368da50d3351b4dbd73c4242
thefarwolf <seth.h.catts@gmail.com>
2018-03-15 22:04:46 -0400

    listing chapter 7 challenges

--- Challenges/Chapter 7 Challenge.py
+++ Challenges/Chapter 7 Challenge.py
@@ -1,1 +1,11 @@
-#Code
+#-1 Make a list and print each value in it using a loop
+
+#-2 Print all the numbers between 25 - 50
+
+#-3 Print each item and its index in a list
+
+#-4 Using an infinite loop write a number guessing game with the
+#   option to enter Q to quit the loop
+
+#-5 Multiply all the number in listA into their same index in
+#   listB and add each to their index in listC
