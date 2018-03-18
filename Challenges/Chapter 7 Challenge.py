5a5999cda96ab2c8c7bf8986cb36802f774ee2ea
thefarwolf <seth.h.catts@gmail.com>
2018-03-17 23:32:30 -0400

    Finishing chapter 7 challenges

--- Challenges/Chapter 7 Challenge.py
+++ Challenges/Chapter 7 Challenge.py
@@ -1,3 +1,4 @@
+import random
 #-1 Make a list and print each value in it using a loop
 def printList(list):
     for el in list:
@@ -7,19 +8,23 @@ def printRange(a, b):
     for i in range(a, b):
         print(i)
 #-3 Print each item and its index in a list
-#TEST
 def printItemAndIndex(list):
-    for i in list.length():
-        print(i + " : " list[i])
+    for i in range(0, len(list)):
+        print(i, ":", list[i])
 #-4 Using an infinite loop write a number guessing game with the
 #   option to enter Q to quit the loop
 def guessTheNumber():
-    //randomNumber = Math.random()
+    randomNumber = random.randint(0, 10)
+    print(randomNumber)
     play = True
     while(play):
         guess = input("Guess a number(1-9): ")
-        if guess == randomNumber:
+        print(guess)
+
+        #This crashes on 'q' input because you cant convert to int
+        if(int(guess) == randomNumber):
             print("That's correct!")
+            play = False
         elif guess == "q":
             play = False
         else:
@@ -29,8 +34,15 @@ def guessTheNumber():
 #   listB and add each to their index in listC
 def multiplyTwoListAndReturnResult(listA, listB):
     listC = []
-    for i in listA.length():
+    for i in range(0, len(listA)):
         listC.append(listA[i] * listB[i])
     print(listC)
 
 #FUNCTION CALLS
+listList = [1,2,3,4,5,6,7,8,9]
+listList0 = [1,2,3,4,5,6,7,8,9]
+#printList(listList)
+#printRange(25,51)
+#printItemAndIndex(listList)
+#guessTheNumber()
+#multiplyTwoListAndReturnResult(listList, listList0)
