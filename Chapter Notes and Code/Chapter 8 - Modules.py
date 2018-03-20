6074367a2ec44a2cc837db2904625db9e61985f9
Seth <seth.h.catts@gmail.com>
2018-03-20 19:04:25 -0400

    Working on chapter 8 files

--- Chapter Notes and Code/Chapter 8 - Modules.py
+++ Chapter Notes and Code/Chapter 8 - Modules.py
@@ -2,25 +2,34 @@
 #importing modules
 import math
 math.pow(2, 3)
-math.randint(0, 10)
+import random
+random.randint(0, 10)
+
+#Aliasing MODULES
+import math as m
+m.pow(2, 3)
 
 #Statistics
 #this is bad form, imports should all be at the top of the doc
 #but im putting the import here for easier reference.
-import statistics
+#import statistics
 nums = [1,2,3,4,5,6]
 #mean
-statistics.mean(nums)
+#statistics.mean(nums)
 #median
-statistics.median(nums)
+#statistics.median(nums)
 #mode
-statistics.mode(nums)
+#statistics.mode(nums)
 
 #keyword module
 import keyword
 keyword.iskeyword("for") #-> True
 keyword.iskeyword("parish") #-> False
 
-#importing written code
-import Chapter 8 - Modules_2.py
-print(Chapter 8 - Modules_2.py.fun())
+#importing written code(AAAAAAAAAaaaaaaand I have to change my file naming
+#convention...)
+import Chapter_8_Modules_2 as c8x
+print(c8x.fun())
+
+"""There's other notes here about stopping loose code from running when it's
+imported, shouldn't be a a probem for me because I'm not slob"""
