1e539fc86c4313452ded2c1941a4ee0c8cf82054
thefarwolf <seth.h.catts@gmail.com>
2018-03-24 22:56:39 -0400

    Trying out some code scraps

--- fnTesting.py
+++ fnTesting.py
@@ -1,7 +1,16 @@
-def test(a, b, c):
-    return (a + b + c)
-
-
-#Calls
-print(test(1,2,3))
-input() #Stop auto close
+def guessTheWord():
+    image = [["          "],
+             ["|-------  "],
+             ["|      |  "],
+             ["      ('')"],
+             ["      \[]/"],
+             ["       || "],
+             ["          "]]
+    incorrectGuesses = 0
+    guessed = False;
+    while !guessed:
+        guess = input("Letter: ")
+        if(word.contains("guess")):
+            #Add to letters guessed
+        else:
+            incorrectGuesses++
