83908fde7964a96d6ebb18825d2d6dd3e5399c15
thefarwolf <seth.h.catts@gmail.com>
2018-03-25 23:37:10 -0400

    More hangman scraps

--- fnTesting.py
+++ fnTesting.py
@@ -1,4 +1,22 @@
+import math
+
+wordList = ["Cat", "Dog", "Pet", "Net", "Ten", "Cap", "Man"]
+
+#This should return a multDem list 
+#filled out to the length of [word]
+def newWord():
+    return wordList(math.Random(0, len(wordList)))
+def returnWordLists(word):
+    length = len(word)
+    retLists = [[],[]]
+    for idx in 2:
+        for i in length:
+            retLists[idx][i].append(0)
+
+#Hangman main
 def guessTheWord():
+    word = newWord()
+    wordLists = returnWordLists(word)
     image = [["          "],
              ["|-------  "],
              ["|      |  "],
