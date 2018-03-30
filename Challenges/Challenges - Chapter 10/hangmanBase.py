037d68a54ecef1ab0c026036626570bde3c98110
Seth <seth.h.catts@gmail.com>
2018-03-29 21:57:40 -0400

    Setting up challenge code

--- Challenges/Challenges - Chapter 10/hangmanBase.py
+++ Challenges/Challenges - Chapter 10/hangmanBase.py
@@ -1,1 +1,37 @@
-#..
+#Modify this base code so that it uses a randomly generated word from a list
+def hangman(word):
+    wrong  = 0
+    stages = ["",
+              "_________          ",
+              "|        |         ",
+              "|      ('')        ",
+              "|     / || \       ",
+              "|      /  \        ",
+              "|                  ",
+              "|                  ",]
+    rLetters = list(word)
+    board = ["__"] * len(word)
+    win = False
+    print("Welcome to Hangman")
+    while(wrong < len(stages) - 1):
+        print("\n")
+        msg = "Guess a letter: "
+        char = raw_input(msg)
+        if char in rLetters:
+            cind = rLetters.index(char)
+            board[cind] = char
+            rLetters[cind] = '$'
+        else:
+            wrong += 1
+        print((" ".join(board)))
+        e = wrong + 1
+        print("\n".join(stages[0: e]))
+        if "__" not in board:
+            print("You win!")
+            print(" ".join(board))
+            win = True
+            break
+    if not win:
+        print("\n".join(stages))
+        print("You lose! The word was {}.".format(word))
+hangman("Power")
