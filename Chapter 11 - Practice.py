3e1c987d7a965055e3b841538564ed785bfe0e0e
Seth <seth.h.catts@gmail.com>
2018-03-31 17:25:42 -0400

    Starting calculator code project

--- Chapter 11 - Practice.py
+++ Chapter 11 - Practice.py
@@ -1,1 +1,21 @@
+#Two variable calculator
+# TODO: usage loop for main function
+# TODO: adding, subtracting, dividing, multiplying
+# TODO: set variable operators function
+
 def calc():
+    usageLoop()
+    print("Calculator closed.")
+
+def usageLoop():
+    vars = setVariables()
+    #ask and operate
+    print("1 - Adding, 2 - Subtracting, 3 - Dividing, 4 - Multiplying")
+    oper = input("Operation: ")
+    if(oper = 1):
+        
+
+def setVariables():
+    x = input("Set A: ")
+    y = input("Set B: ")
+    return [x,y]
