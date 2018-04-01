56ee18a61e5e7b6bde01bb3988016f3a45e7dd93
thefarwolf <seth.h.catts@gmail.com>
2018-04-01 19:14:36 -0400

    Working on chapter 11 calc

--- Chapter 11 - Practice.py
+++ Chapter 11 - Practice.py
@@ -12,8 +12,15 @@ def usageLoop():
     #ask and operate
     print("1 - Adding, 2 - Subtracting, 3 - Dividing, 4 - Multiplying")
     oper = input("Operation: ")
-    if(oper = 1):
-        
+    if(oper == 1):
+        print(add(vars))
+    elif(oper == 2):
+        print(sub(vars))
+    elif(oper == 3):
+        print(div(vars))
+    elif(oper == 4):
+        print(mul(vars))
+
 
 def setVariables():
     x = input("Set A: ")
