4a53e4d7cee4ad2944cf7b7d00e571a48daabc7a
thefarwolf <seth.h.catts@gmail.com>
2018-04-24 23:00:33 -0400

    Completing challenge #4

--- Challenges/Challenges - Chapter 12/C4_Hexagon.py
+++ Challenges/Challenges - Chapter 12/C4_Hexagon.py
@@ -1,1 +1,9 @@
 #Create Hexagon object, create a method to return parimeter
+class hexagon():
+    def __init__(self, l):
+        self.side_length = l
+        self.parimeter = (l * 6)
+    def get_parimeter(self):
+        return self.parimeter
+newHex = hexagon(4)
+print(newHex.get_parimeter())
