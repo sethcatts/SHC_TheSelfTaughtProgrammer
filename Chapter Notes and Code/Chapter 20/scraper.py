bd3c93479767f35d087fff8f79e9b3df58478ae6
thefarwolf <seth.h.catts@gmail.com>
2018-07-13 02:30:37 -0400

    tweak

--- Chapter Notes and Code/Chapter 20/scraper.py
+++ Chapter Notes and Code/Chapter 20/scraper.py
@@ -15,9 +15,24 @@ class Scraper:
             #print(url)
             if url is None:
                 continue
-            if "html" & "trump" in url:
+            if "html" in url:
                 print("\n" + url)
 
 news = "https://news.yahoo.com/"
 
 Scraper(news).scrape()
+
+class Scraper:
+    def __init__(self, site):
+        self.site = site
+
+    def scrape(self):
+        response = urllib.request.urlopen(self.site)
+        html = response.read()
+        soup = BeautifulSoup(html, 'html.parser')
+        with open("output.txt", "w") as f:
+            for tag in soup.find_all('a'):
+                url = tag.get('href')
+                if url and 'html' in url:
+                    print("\n" + url)
+                    f.write(url + "\n")
